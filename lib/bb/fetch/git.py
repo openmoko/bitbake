44c130c8f7acf8bc137bbc18bae5aeecfa08b7b0
Julian_Chu <julian_chu@openmoko.com>
2008-10-07 19:19:34 +0800

    git.py : Failing to remove a directory fails for symlinks     The packagekit repository contains a symlink and prunedir did not     cope with it. Fix that error.

--- lib/bb/fetch/git.py
+++ lib/bb/fetch/git.py
@@ -34,7 +34,10 @@ def prunedir(topdir):
         for name in files:
             os.remove(os.path.join(root, name))
         for name in dirs:
-            os.rmdir(os.path.join(root, name))
+            if os.path.islink(os.path.join(root, name)):
+                os.remove(os.path.join(root, name))
+            else:
+                os.rmdir(os.path.join(root, name))
 
 class Git(Fetch):
     """Class to fetch a module or modules from git repositories"""
